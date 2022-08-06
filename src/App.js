f4d8ffe244065d0b8ce17e334c575d4940d60bd4
Daria <badrenkovadasha@yandex.ru>
2022-08-07 01:43:18 +0300

    homework 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
+import { useEffect, useState } from 'react';
 import appStyle from './App.module.css';
 import { Message } from './Message';
-
-
+const setMessage = (value = '', author = 'User') => {
+  return {
+    text: value,
+    author: author,
+  };
+};
 
 export function App() {
-  const message = 'Hello World!'
+  const [messageList, setMessageList] = useState([]);
+  // const [buttonClicks, setButtonClicks] = useState(0);
+  const [value, setValue] = useState('');
+  useEffect(() => {
+    if (
+      messageList.length > 0 &&
+      messageList[messageList.length - 1].author != 'Robot'
+    ) {
+      setTimeout(
+        () =>
+          setMessageList([...messageList, setMessage('I am a robot', 'Robot')]),
+        1500
+      );
+    }
+  }, [messageList]);
+
   return (
     <div className={appStyle.App}>
-      <header className={appStyle.AppHeader}>
-        <Message message={message} />
-      </header>
+      <div>
+        {messageList.map((item) => (
+          <Message text={item.text} author={item.author} />
+        ))}
+      </div>
+      <div className={appStyle.areaForWriting}>
+        <input
+          className={appStyle.input}
+          placeholder="Message"
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        ></input>
+        <button
+          className={appStyle.sendBtn}
+          onClick={() => {
+            if (value !== '') {
+              setMessageList([...messageList, setMessage(value)]);
+              // setButtonClicks(buttonClicks + 1);
+              setValue('');
+            }
+          }}
+        ></button>
+      </div>
     </div>
   );
 }
-
--- src/Message.js
+++ src/Message.js
@@ -1,7 +1,15 @@
-import messageStyle from './Message.module.css'
+import messageStyle from './Message.module.css';
 
-export const Message = ({ message }) => {
-    return (
-        <p className={messageStyle.Message}>{message}</p>
-    )
-}
+export const Message = ({ text, author, time }) => {
+  const classes = `${messageStyle.message} ${
+    author === 'User' ? messageStyle.messageUser : messageStyle.messageOthers
+  }`;
+  return (
+    <div className={messageStyle.messageWrapper}>
+      <div className={classes}>
+        {/* <span className={messageStyle.MessageAuthor}>{author}</span> */}
+        <span className={messageStyle.MessageText}>{text}</span>
+      </div>
+    </div>
+  );
+};
